c14fbf8f033028882f3c9730ad9b9b87ab0872c4
Emmanuel Joshua <iamemmanueljoshua@gmail.com>
2022-03-15 18:39:26 -0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,62 @@
+# run script in Agora's top-level directory
+# test_hw.sh [client output file] [base station output file]
+#!/bin/bash
+​
+ue_out_file=$1
+bs_out_file=$2
+​
+echo "==========================================="
+echo "Build cmake... with -DUSE_ARGOS=on -DUSE_UHD=off"
+echo "==========================================="
+cmake -S ./ -B ./build -DUSE_ARGOS=on -DUSE_UHD=off
+echo -e "-------------------------------------------------------\n\n\n"
+​
+echo "==========================================="
+echo "execute make"
+echo "==========================================="
+make -j -C ./build
+echo -e "-------------------------------------------------------\n\n\n"
+​
+echo "==========================================="
+echo "Generating data for test"
+echo "==========================================="
+./build/data_generator --conf_file data/ue-ul-hw.json
+echo -e "-------------------------------------------------------\n\n\n"
+​
+echo "==========================================="
+echo "Starting clients"
+echo "==========================================="
+echo -e "-------------------------------------------------------\n\n\n"
+​
+for i in 1 2 3; do
+{  # try to start radio at most three times
+​
+    ./build/user data/ue-ul-hw.json > $ue_out_file &
+    pid=$!
+    sleep 1
+    timeout 8 tail -f -n0 $ue_out_file | grep -qe "radio start done!"
+    if [ $? != 0 ]; then
+        if [ $i == 3 ]; then
+            echo "==========================================="
+            echo "cannot start radio, exiting..."
+            echo "==========================================="
+            kill $pid >/dev/null
+            exit
+        fi
+        echo "==========================================="
+        echo "radio did not start, trying again..."
+        echo "===========================================" 
+        kill $pid >/dev/null
+        else
+            sleep 2
+            break
+    fi
+} done
+​
+echo ""
+echo "==========================================="
+echo "Starting base stations"
+echo "==========================================="
+./build/agora data/bs-ul-hw.json > $bs_out_file &
+wait
+exit
